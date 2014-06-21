4e53d9a1bbb33814e8955aac080d077b9e1bf545
Ben Kamphaus <bkamphaus@cognitect.com>
2014-06-20 18:45:01 -0600

    Added python tool to mirror settings of current user.

---
+++ match_config.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python
+from os.path import expanduser, sep, dirname, basename
+from glob import glob
+from shutil import copy
+
+# Get paths and files.
+this_dir = dirname(__file__)
+dotfiles = glob(this_dir + sep + ".*")
+homedir = expanduser("~")
+
+# Get list of src files and copy them to dotfiles directory.
+src_files = [homedir + sep + basename(dotfile) for dotfile in dotfiles]
+for src_file in src_files:
+    try:
+        copy(src_file, this_dir + sep + basename(src_file))
+    except IOError:
+        print("Skipping " + src_file + " b/c it's not present in deployment.")
