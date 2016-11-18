02224736b1e0f2c06a37e700c92b5f177a10d4c2
Ben Kamphaus <ben.kamphaus@gmail.com>
2016-11-17 20:44:38 -0700

    [lein] clean up global dev profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,9 @@
-{:user {
-        :plugins ;[[venantius/ultra "0.2.1"]]
-                 [[cider/cider-nrepl "0.8.2"]]
-;        :ultra {:color-scheme :solarized_dark}
-        :repl-options {:prompt (fn [ns] (str "\033[1;32m"
-                                         "<clj> [" ns "]$ " "\033[0m "))}}}
+{:user {:plugins [[venantius/ultra "0.5.0"]
+                  [cider/cider-nrepl "0.14.0"]]
+        :ultra {:color-scheme :solarized_dark}
+        :repl-options {:prompt (fn [ns]
+                                 (str "\033[1;32m"
+                                      "<clj> ["
+                                      ns
+                                      "]$
+                                      " "\033[0m "))}}}
